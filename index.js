a282ee6b7cf881dd51ab85c14bccf8ab3a25c44f
Tatum Daily <tgd9019@outlook.com>
2020-05-31 12:57:27 -0500

    Cube ready

---
+++ index.js
@@ -0,0 +1,7 @@
+function myPlayFunction() {
+    document.getElementById("cube").style.animationPlayState = "running";
+  }
+  
+  function myPauseFunction() {
+    document.getElementById("cube").style.animationPlayState = "paused";
+  }
